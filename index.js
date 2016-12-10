8ed7d15fcbc93a1021c0afc15a06d862f471cb56
feizheng <1290657123@qq.com>
2016-12-10 14:29:51 +0800

    update

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
 var ROOT_PATH = require('root-path');
+var semver = require('semver');
+var chalk = require('chalk');
+var packageJson = require(ROOT_PATH + '/package.json');
+var exec = function(cmd) {
+  return require('child_process')
+    .execSync(cmd).toString().trim()
+};
 
-//enjoy coding...
+
+var versionRequirements = [{
+  name: 'node',
+  currentVersion: semver.clean(process.version),
+  versionRequirement: packageJson.engines.node
+}, {
+  name: 'npm',
+  currentVersion: exec('npm --version'),
+  versionRequirement: packageJson.engines.npm
+}];
+
+module.exports = function() {
+  var warnings = [];
+  for (var i = 0; i < versionRequirements.length; i++) {
+    var mod = versionRequirements[i];
+    if (!semver.satisfies(mod.currentVersion, mod.versionRequirement)) {
+      warnings.push(mod.name + ': ' +
+        chalk.red(mod.currentVersion) + ' should be ' +
+        chalk.green(mod.versionRequirement)
+      );
+    }
+  }
+
+  if (warnings.length) {
+    console.log('')
+    console.log(chalk.yellow('To use this template, you must update following to modules:'))
+    console.log()
+    for (var i = 0; i < warnings.length; i++) {
+      var warning = warnings[i]
+      console.log('  ' + warning);
+    }
+    process.exit(1);
+  }
+};
